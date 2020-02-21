ae8ac5cb0e7bccc51abd7dff6b1f98326ad4bd66
Radik12 <polyanscky.rodion@ya.ru>
2020-02-21 21:28:35 +0300

    Added functions which find shortest path

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"container/list"
+	"encoding/csv"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,6 +10,14 @@ import (
 	"log"
 	"os"
 )
+
+const (
+	StartParent string = "startParent"
+)
+
+type void struct{}
+
+var member void
 
 type Subscriber struct {
 	Email     string `json:"Email"`
@@ -21,7 +31,7 @@ type User struct {
 	Subs      []Subscriber `json:"Subscribers"`
 }
 
-func readJSONFile(filename string) ([]User, error) {
+func readJSONFile(filename string) ([]*User, error) {
 	f, err := os.Open(filename)
 	if err != nil {
 		return nil, errors.New("unable to read input json file " + filename)
@@ -33,10 +43,88 @@ func readJSONFile(filename string) ([]User, error) {
 		return nil, errors.New("unable to read input json file as a byte array " + filename)
 	}
 
-	users := make([]User, 0)
+	var users []*User
 	json.Unmarshal(byteData, &users)
 
 	return users, nil
+}
+
+func readCSVFile(filePath string) ([][]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, errors.New("unable to read input file " + filePath)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+
+	if err != nil {
+		return nil, errors.New("unable to parse file as CSV for " + filePath)
+	}
+
+	return records, nil
+}
+
+func parseContacts(users []*User) map[string][]string {
+	contacts := make(map[string][]string)
+	for _, user := range users {
+		contacts[user.Email] = make([]string, 0)
+		for _, sub := range user.Subs {
+			contacts[user.Email] = append(contacts[user.Email], sub.Email)
+		}
+	}
+
+	return contacts
+}
+
+func breadthFirstSearch(start string, contacts map[string][]string) (map[string]void, map[string]string) {
+	visited := make(map[string]void)
+	visited[start] = member
+
+	parent := make(map[string]string)
+	parent[start] = StartParent
+
+	queue := list.New()
+	queue.PushBack(start)
+
+	for queue.Len() > 0 {
+		node := queue.Front()
+		email := node.Value.(string)
+		for _, to := range contacts[email] {
+			if _, ok := visited[to]; !ok {
+				visited[to] = member
+				queue.PushBack(to)
+				parent[to] = email
+			}
+		}
+		queue.Remove(node)
+	}
+
+	return visited, parent
+}
+
+func reversePath(path []string) {
+	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+		path[i], path[j] = path[j], path[i]
+	}
+}
+
+func findShortestPath(from string, to string, contacts map[string][]string) []string {
+	visited, parent := breadthFirstSearch(from, contacts)
+
+	if _, ok := visited[to]; !ok {
+		return nil
+	}
+
+	path := make([]string, 0)
+	for v := to; v != StartParent; v = parent[v] {
+		path = append(path, v)
+	}
+
+	reversePath(path)
+
+	return path
 }
 
 func main() {
@@ -45,7 +133,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < len(users); i++ {
-		fmt.Println(users[i].Email)
+	contacts := parseContacts(users)
+
+	path := findShortestPath("seal969@rambler.ru", "tomcat1445@rambler.ru", contacts)
+
+	for _, el := range path {
+		fmt.Println(el)
 	}
 }
